deeccc207ee716ca3164b2bca8eb65f9b60bbc39
dude3133 <dude33133@gmail.com>
2015-12-14 01:37:06 +0200

    Added Test Project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MappersTests 
+    {
+        
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public  void TruncatedUniversityMapper_Test()
+        {
+            var result = 1;
+            var expected = 1;
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
